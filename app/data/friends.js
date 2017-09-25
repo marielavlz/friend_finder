e9153a0e20706b47140dc63372b6017759d828f8
Mariela Velez <Mariela@marielas-air.attlocal.net>
2017-09-24 23:45:08 -0500

    Finished the basic functionality of the app but still have to change the UI and deploy to Heroku.

---
+++ app/data/friends.js
@@ -0,0 +1,95 @@
+//Starter data for friend_finder
+var friends = [{
+	name:"Chandler Bing",
+	photo:"https://img1.telestar.fr/var/telestar/storage/images/media/images/2016/photos/20160324-friends-matthew/matthew-perry4/1301377-1-fre-FR/Matthew-Perry_width1024.jpg",
+	scores:[
+		3,
+		1,
+		2,
+		1,
+		2,
+		5,
+		2,
+		1,
+		3,
+		5
+	]
+},{
+	name:"Pheobe Buffay",
+	photo:"https://pbs.twimg.com/media/DF_wtIrW0AEHJci.jpg",
+	scores:[
+		2,
+		4,
+		1,
+		1,
+		3,
+		1,
+		1,
+		5,
+		2,
+		4
+	]
+},{
+	name:"Rachel Green",
+	photo:"http://ilarge.lisimg.com/image/6306371/1118full-rachel-green.jpg",
+	scores:[
+		3,
+		4,
+		5,
+		1,
+		5,
+		2,
+		4,
+		3,
+		1,
+		1
+	]
+},{
+	name:"Monica Geller",
+	photo:"http://22dakika.org/wp-content/uploads/2014/04/tumblr_mmrnncoZFF1r2vo64o1_1280.jpg",
+	scores:[
+		5,
+		3,
+		3,
+		1,
+		4,
+		2,
+		5,
+		2,
+		5,
+		1
+	]
+},{
+	name:"Ross Geller",
+	photo:"https://img3.telestar.fr/var/telestar/storage/images/media/images/2016/photos/20161102-david-schwimmer/david-schwimmer-dans-friends-en-2004-pour-la-derniere-saison-10/1510212-1-fre-FR/David-Schwimmer-dans-Friends-en-2004-pour-la-derniere-saison-10_width1024.jpg",
+	scores:[
+		4,
+		2,
+		3,
+		5,
+		1,
+		3,
+		4,
+		1,
+		3,
+		1
+	]
+},{
+	name:"Joey Tribbiani",
+	photo:"https://vignette2.wikia.nocookie.net/friends/images/5/5a/Square_Joey.jpg/revision/latest?cb=20160528174812",
+	scores:[
+		1,
+		5,
+		3,
+		1,
+		3,
+		2,
+		2,
+		1,
+		5,
+		5
+	]
+}];
+
+//Export data to API
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+//Use Heroku port or local 3000
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Pulling in routes defined under routing folder
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+app.use(express.static("app"));
+
+app.listen(PORT);
